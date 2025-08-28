f7193409b22564c6d02e8760a57f303624809e22
AnuJain62 <anujain.aj021@gmail.com>
2025-08-28 17:36:03 +0530

    Front end

---
+++ Java Script/Variables, Data Type, Operators/script.js
@@ -0,0 +1,41 @@
+// variables 
+// dynamic typing - it means here we does not required defined the type of data
+
+// var a = 78;
+// var b = "harry";
+// console.log(a);
+// console.log(b);
+
+
+
+// operand - entities on which operators operate
+// Operators 
+// 1) unary operator - it has single operand (eg. x = -x)
+// 2) binary operator - it has two operand (eg. x = x + 6)
+
+// unary
+// var c = 34.55;
+// console.log(c);
+// c = -c;
+// console.log(c);
+
+// binary
+// c = 456 + 8;
+// console.log(c);
+
+
+
+// arithmetic operators
+var num1 = 2;
+var num2 = 6;
+console.log("the value of num1 + num2 is "+(num1+num2));
+console.log("the value of num1 - num2 is "+(num1-num2));
+console.log("the value of num1 * num2 is "+(num1*num2));
+console.log("the value of num1 / num2 is "+(num1/num2));
+console.log("the value of num1 ** num2 is "+(num1**num2));
+
+
+
+
+
+
