0fcd1c73fdffc2f01b5924fc97b520e1aa3c91e5
AnuJain62 <anujain.aj021@gmail.com>
2025-08-28 18:37:23 +0530

    Front end

---
+++ Java Script/strings/script.js
@@ -0,0 +1,43 @@
+
+// var string1 = "this";
+// var string2 = 'there';
+// console.log(string1);
+// console.log(string2);
+
+
+// it gives error
+// var string = 'thi's';    
+// var string = "thi"s";
+
+
+// var string = 'thi"s';
+// var name = 'harry';
+// var message = 'harry is a good boy';
+// console.log(string + name + message);
+
+// var channel = 'CodeWithHarry';
+// var temp = `${name} is a 'nice' person "and" he has a channel called ${channel}`;
+// console.log(temp);
+
+// var len = name.length;
+// console.log(`Length of name is ${len}`);
+
+// // for new line
+// console.log(`Length of name is\n ${len}`);
+
+// // for one tab
+// console.log(`Length of name is\n ${len}`);
+
+
+var y = new String("this");
+console.log(y);
+
+// it gives error
+// document.getElementById('content').innerHTML = '<h3> this is an h3 heading <h3>'
+
+
+
+
+
+
+
