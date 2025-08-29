7d7c63380939f0179d97f0a37c2a8e6f913558f3
AnuJain62 <anujain.aj021@gmail.com>
2025-08-29 22:44:48 +0530

    Front end

---
+++ Java Script/Loops/script.js
@@ -0,0 +1,57 @@
+console.log("hello");
+
+
+// for loop
+// let i;
+// for(i=0;i<3;i++)
+// console.log(i);
+
+
+// for-each loop
+// let friends = ["rohan", "sanjeev", "deepti", "pooja", "skillf"];
+// friends.forEach(function f(element){
+//  console.log("hello friend "+element+" to moder Js");
+// });
+
+
+// for-of loop
+// let friends = ["rohan", "sanjeev", "deepti", "pooja", "skillf"];
+// for (element of friends){
+//  console.log("hello friend "+element+" to moder Js");
+// }
+
+
+// let employee = {
+//  name: "harry",
+//  salary: 2,
+//  channel: "CWH"
+// }
+// // use this loop of iterate over objects in Js
+// for(key in employee){
+//  console.log(`the ${key} of employee is ${employee[key]}`);
+// }
+
+
+
+// while loop
+// let i=0;
+// while(i<4){
+//  console.log(`${i} is less than 4`);
+//  i++;
+// }
+
+
+
+// do-while loop
+let i=34;
+do{
+ console.log(`${i} is less than 4 and we are inside do while loop`);
+ i++;
+}while(i<4);
+
+
+
+
+
+
+
